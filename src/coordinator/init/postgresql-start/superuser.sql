40ea566c833e2b62418e7566e8fd9cf5e02a0304
Adam C Hamlin <achamlin@gmail.com>
2019-11-19 23:41:14 -0500

    Add source tracking aggregator
    - Add agg_source custom source column aggregator and add_sources custom function
    - Wrap foreign tables in view that intitializes source column
    - Add use_remote_estimate=true for foreign servers
    - Expose contractor's pg_stat_user_tables view thru fdw

--- src/coordinator/init/postgresql-start/superuser.sql
+++ src/coordinator/init/postgresql-start/superuser.sql
@@ -4,3 +4,61 @@
 
 CREATE EXTENSION source_tracking_fdw;
 CREATE EXTENSION postgres_fdw;
+
+-- Custom aggregator for json source column
+drop aggregate if exists agg_source(jsonb);
+drop function if exists agg_source_finalfunc(jsonb);
+drop function if exists agg_source_sfunc(jsonb, jsonb);
+
+create function agg_source_sfunc(agg_state jsonb, el jsonb)
+returns jsonb
+immutable
+language plpgsql
+as $$
+declare
+  src text;
+  cnt int;
+  sum int;
+begin
+  for src, cnt in select * from jsonb_each_text(el) loop
+    if agg_state->>src is null then
+      sum := cnt;
+    else
+      sum := (agg_state->>src)::int + cnt;
+    end if;
+    agg_state := agg_state || ('{ "' || src || '": ' || sum || ' }')::jsonb;
+  end loop;
+
+  return agg_state;
+end;
+$$;
+
+create function add_sources(a jsonb, b jsonb) -- just aliasing above function
+returns jsonb
+immutable
+strict
+language plpgsql
+as $$
+begin
+  return agg_source_sfunc(a, b);
+end;
+$$;
+
+create function agg_source_finalfunc(agg_state jsonb)
+returns jsonb
+immutable
+strict
+language plpgsql
+as $$
+begin
+  return agg_state;
+end;
+$$;
+
+create aggregate agg_source (jsonb)
+(
+    sfunc = agg_source_sfunc,
+    stype = jsonb,
+    finalfunc = agg_source_finalfunc,
+    initcond = '{}'
+);
