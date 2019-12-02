26343620f82efc89996007cc0302a0b798fffc1b
dfirer <dfirer@terpmail.umd.edu>
2019-12-02 18:48:44 -0500

    accesses function without excess

--- src/coordinator/init/postgresql-start/superuser.sql
+++ src/coordinator/init/postgresql-start/superuser.sql
@@ -90,30 +90,3 @@ begin
   end if;  
 end;
 $$;
--- create a function to find how many times the table has been accessed
-create function accesses_1(table_name text, contractor text)
-returns numeric
-language plpgsql
-as $$
-declare
-  returned record;
-  seq bigint;
-  idx bigint;
-  place int;
-  cnt int := 0;
-begin
-  --with tot as
-  --(select reltuples::bigint from pg_class where relname= table_name)
-  EXECUTE format('SELECT seq_scan, idx_scan FROM %s where relname = %s', contractor||'.pg_stat_user_tables', table_name)
-  INTO seq, idx;
-  place := seq + ceil(cast(idx as double precision)/ 10.0);
-  while place >= 10^cnt loop
-    cnt := cnt + 1;
-  end loop;
-  if cnt <= 5 then
-    return cnt * 1;
-  else
-    return cnt * 15;
-  end if;  
-end;
-$$;
